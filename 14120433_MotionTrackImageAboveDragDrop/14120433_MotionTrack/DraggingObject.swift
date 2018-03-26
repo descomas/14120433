60a09a15b365ed03cadc5daaa9c97fe0c0316a7d
14120433 <desireect@live.com>
2018-03-26 23:05:15 +0100

    Add files via upload
    Add main character object and can move

---
+++ 14120433_MotionTrackImageAboveDragDrop/14120433_MotionTrack/DraggingObject.swift
@@ -0,0 +1,27 @@
+//
+//  DraggingObject.swift
+//  14120433_MotionTrack
+//
+//  Created by Desiree Comas on 23/03/2018.
+//  Copyright © 2018 Desiree Comas. All rights reserved.
+//
+
+import UIKit
+
+class DraggingObject: UIImageView {
+
+    var startLocation: CGPoint?
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        startLocation = touches.first?.location(in: self)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let currentLocation = touches.first?.location(in: self)
+        let dx = currentLocation!.x - startLocation!.x
+        let dy = currentLocation!.y - startLocation!.y
+        
+        self.center = CGPoint(x: self.center.x+dx, y: self.center.y+dy)
+    }
+
+}
